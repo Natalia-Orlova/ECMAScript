ba6afa6eb03ccb434346987f7de441a91a44ce39
NataliaOrlova <natakarelskaya@yandex.ru>
2023-10-15 00:20:48 +0400

    added solution hw1

--- lesson1/script.js
+++ lesson1/script.js
@@ -12,7 +12,30 @@ console.log(Math.min(...[1, 5, 7, 9]));
 // на 1. Значение счетчика должно быть доступно только через методы объекта,
 // а не напрямую.
 
+function createCounter() {
+  let counter = 0;
+  return {
+    increment() {
+      counter++;
+    },
+    decrement() {
+      counter--;
+    },
+    getValue() {
+      return counter;
+    },
+  };
+}
 
+const num = createCounter();
+num.increment();
+console.log(`1-й счетчик: ${num.getValue()}`);
+num.increment();
+console.log(`1-й счетчик: ${num.getValue()}`);
+
+const num2 = createCounter();
+num2.increment();
+console.log(`2-й счетчик: ${num2.getValue() * 4}`);
 
 // 3) Дополнительное задание, выполняем только если проходили работу с DOM.
 // Напишите рекурсивную функцию findElementByClass, которая принимает корневой
@@ -23,3 +46,20 @@ console.log(Math.min(...[1, 5, 7, 9]));
 // const rootElement = document.getElementById('root');
 // const targetElement = findElementByClass(rootElement, 'my-class');
 // console.log(targetElement);
+
+let result = null;
+function findElementByClass(rootElement, classElement) {
+  if (rootElement.hasChildNodes()) {
+    for (const element of rootElement.children) {
+      if (element.className === classElement) {
+        result = element;
+      } 
+      findElementByClass(element, classElement);
+    }
+  }
+  return result;
+}
+
+const rootElement = document.getElementById("root");
+const targetElement = findElementByClass(rootElement, "my-class");
+console.log(targetElement);
