a525be336a4a7bded5a0c16ea45a1ce3f5990933
NataliaOrlova <natakarelskaya@yandex.ru>
2023-10-26 22:57:14 +0400

    task2

--- lesson4/script.js
+++ lesson4/script.js
@@ -20,12 +20,11 @@ async function getUserData(ID) {
 }
 
 getUserData(9).then(user => {
-    user !== undefined ? console.log(user) 
+    user !== undefined ? console.log(user)
         : console.log('There is no user with such ID');
     }).catch(e => {
     	console.log(e)
     })
-
 
 // Задание 2. Отправка данных на сервер.
 
@@ -33,22 +32,38 @@ getUserData(9).then(user => {
 
 // *Подсказка *
 
-// // Пример использования функции
-// const user = {
-//   "name": "John Doe",
-//   "job": "unknown"
-// };
-
-// saveUserData(user)
-//   .then(() => {
-//     console.log('User data saved successfully');
-//   })
-//   .catch(error => {
-//     console.log(error.message);
-//   });
 // saveUserData использует fetch для отправки данных о пользователе на удаленный сервер для сохранения. Она отправляет POST-запрос на URL-адрес /api/users с указанием типа содержимого application/json и сериализует объект с данными о пользователе в JSON-строку с помощью JSON.stringify(). Если запрос успешен (с кодом 201), функция разрешает промис. Если запрос неуспешен, функция отклоняет промис с сообщением об ошибке.
 
 // Работа должна быть выполнена с API: https://reqres.in/
+
+async function saveUserData(user) {
+  const response = await fetch("https://reqres.in/api/users", {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json;charset=UTF-8",
+    },
+    body: JSON.stringify(user),
+  });
+  if (response.ok) {
+    return await response.json();
+  } else {
+    throw new Error(`Error status ${response.status}`);
+  }
+}
+
+// // Пример использования функции
+const user = {
+  "first_name": "Bob",
+  "last_name": "Dylan",
+};
+
+saveUserData(user)
+  .then(() => {
+    console.log("User data saved successfully");
+  })
+  .catch((error) => {
+    console.log(error.message);
+  });
 
 // Задание 3. Изменение стиля элемента через заданное время (выполняем, если знакомы с DOM).
 
